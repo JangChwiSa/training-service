32c1b0ae2165275ab55bc7862218897dc90365d0
Yijungu <wnsrn4970@naver.com>
2026-04-28 14:02:11 +0900

    feat: add project scaffold

---
+++ src/test/java/com/jangchwisa/trainingservice/TrainingServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jangchwisa.trainingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TrainingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
